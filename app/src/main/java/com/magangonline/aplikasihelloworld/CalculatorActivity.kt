5a6ba5fe1d25975972126eddfb27a25cba0ab2b9
Amar Sheriffudin <sheriffudin26@gmail.com>
2021-03-24 16:50:27 +0700

    calculator xml

--- app/src/main/java/com/magangonline/aplikasihelloworld/CalculatorActivity.kt
+++ app/src/main/java/com/magangonline/aplikasihelloworld/CalculatorActivity.kt
@@ -2,15 +2,18 @@ package com.magangonline.aplikasihelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_calculator.*
 
-class CalculatorActivity : AppCompatActivity() {
+class CalculatorActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
 
+//        hitung.setOnClickListener(this)
         hitung.setOnClickListener {
+            tv_hasil.text = "Hasil Penjumlahan"
             val nilai1 = nilai_1.text.toString()
             val nilai2 = nilai_2.text.toString()
 
@@ -19,11 +22,13 @@ class CalculatorActivity : AppCompatActivity() {
             hasil.text = jumlah.toString()
             et_hasil.setText(jumlah.toString())
 
-
         }
 
     }
 
+    override fun onClick(v: View?) {
+
+    }
 
 
 }
